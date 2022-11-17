fbcd948aced71cc898ad5ebb51d17f97a570ce11
Jeronimo Teixeira <geteixeira095@gmail.com>
2022-11-17 00:04:31 -0300

    Initial commit

---
+++ apps/sub-entry-point/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sub-entry-point-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sub-entry-point';
+}
